182b8f921360dfaec7fae75dafa5f05a4601781e
she11sh0cked <22623152+she11sh0cked@users.noreply.github.com>
2021-03-22 22:21:05 +0100

    ➕ Add eslint-plugin-simple-import-sort

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,8 @@
 module.exports = {
   extends: ['react-app', 'react-app/jest', 'prettier'],
+  plugins: ['simple-import-sort'],
+  rules: {
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+  },
 }
