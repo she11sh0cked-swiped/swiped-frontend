f1639c6c952e8842d3c712e740f79a121dc7004c
she11sh0cked <22623152+she11sh0cked@users.noreply.github.com>
2021-03-23 22:22:31 +0100

    🎨 Use different material-ui import style

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,4 @@
-import CssBaseline from '@material-ui/core/CssBaseline'
-import ThemeProvider from '@material-ui/styles/ThemeProvider'
+import { CssBaseline, ThemeProvider } from '@material-ui/core'
 import { FC } from 'react'
 
 import theme from './theme'
