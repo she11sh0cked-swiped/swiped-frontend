2b2257a4bf44910f4eab94c378199a3c830cd166
she11sh0cked <22623152+she11sh0cked@users.noreply.github.com>
2021-03-28 17:26:28 +0200

    🔥 Remove NavigationBase component

--- src/components/navigation/NavigationBase.styles.ts
+++
@@ -1,9 +0,0 @@
-import { makeStyles } from '@material-ui/core'
-
-export default makeStyles({
-  root: {
-    alignItems: 'center',
-    display: 'flex',
-    justifyContent: 'space-between',
-  },
-})
