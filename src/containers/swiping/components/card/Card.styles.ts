e43726ee47cffdcad3a32c0703e677d57c24d592
she11sh0cked <22623152+she11sh0cked@users.noreply.github.com>
2021-03-29 09:06:19 +0200

    ✨ Add swiping card component

---
+++ src/containers/swiping/components/card/Card.styles.ts
@@ -0,0 +1,29 @@
+import { makeStyles } from '@material-ui/core'
+import { common } from '@material-ui/core/colors'
+
+export default makeStyles({
+  content: {
+    backgroundImage: 'linear-gradient(0deg, rgba(0,0,0,0.666), rgba(0,0,0,0))',
+    bottom: 0,
+    color: common.white,
+    position: 'absolute',
+    width: '100%',
+  },
+  info: {
+    bottom: 0,
+    margin: '0.5em',
+    position: 'absolute',
+    right: 0,
+  },
+  root: {
+    '& > .MuiCardActionArea-root': {
+      '& > .MuiCardMedia-root': {
+        height: '100%',
+      },
+
+      height: '100%',
+    },
+
+    height: '100%',
+  },
+})
---
+++ src/containers/swiping/components/card/Card.tsx
@@ -0,0 +1,37 @@
+import {
+  Card as MaterialCard,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Typography,
+} from '@material-ui/core'
+import { Info } from '@material-ui/icons'
+import { FC } from 'react'
+
+import useStyles from './Card.styles'
+
+interface IProps {
+  image: string
+  title: string
+}
+
+const Card: FC<IProps> = ({ image, title }) => {
+  const classes = useStyles()
+
+  return (
+    <MaterialCard className={classes.root}>
+      <CardActionArea>
+        <CardMedia image={image} />
+        <CardContent className={classes.content}>
+          <Typography component="h2" variant="h5">
+            {title}
+          </Typography>
+          <Info className={classes.info} />
+        </CardContent>
+      </CardActionArea>
+    </MaterialCard>
+  )
+}
+
+export type { IProps as ICard }
+export default Card
