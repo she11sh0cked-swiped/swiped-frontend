f5d098ed6b633a61e853fd13462dee66dc7464b5
she11sh0cked <22623152+she11sh0cked@users.noreply.github.com>
2021-03-29 09:08:19 +0200

    ✨ Add route to swiping container

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,20 +7,32 @@ import {
   Toolbar,
 } from '@material-ui/core'
 import { observer } from 'mobx-react'
-import { FC } from 'react'
+import { FC, lazy, Suspense } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
+import Loading from 'containers/loading/Loading'
 import app from 'store/App'
 
 import Navigation from './components/navigation/Navigation'
 import theme from './theme'
 
+const Swiping = lazy(() => import('containers/swiping/Swiping'))
+
 const App: FC = () => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Navigation {...app.navigation} />
-      <Toolbar />
-      <Container maxWidth="sm">coming soon...</Container>
+      <Router>
+        <Navigation {...app.navigation} />
+        <Toolbar />
+        <Container maxWidth="sm">
+          <Suspense fallback={<Loading />}>
+            <Switch>
+              <Route exact path="/:groupId/swiping" component={Swiping} />
+            </Switch>
+          </Suspense>
+        </Container>
+      </Router>
     </ThemeProvider>
   )
 }
