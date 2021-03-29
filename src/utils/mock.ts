aeff09eafb74195ddbd0d7d20cea719cf148f571
she11sh0cked <22623152+she11sh0cked@users.noreply.github.com>
2021-03-30 01:13:08 +0200

    🤡 Use placeimg instead of unsplash

--- src/utils/mock.ts
+++ src/utils/mock.ts
@@ -1,9 +1,16 @@
 const id = (i: number) => `V1StGXR${i}_Z5jdHi6B-myT`
 
-export const movies = Array(10)
-  .fill(null)
-  .map((_, i) => ({
-    id: id(i),
-    image: `https://source.unsplash.com/random?random=${i}`,
-    title: 'Movie Title',
-  }))
+interface IMovie {
+  id: string
+  image: string
+  title: string
+}
+
+export const movies = (): IMovie[] =>
+  Array(10)
+    .fill(null)
+    .map((_, i) => ({
+      id: id(i),
+      image: `https://placeimg.com/1080/1920/any?random=${i}`,
+      title: `${i} Movie Title`,
+    }))
