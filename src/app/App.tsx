06fdade5d5124dddd3e08220e220b68fb7616423
she11sh0cked <22623152+she11sh0cked@users.noreply.github.com>
2021-03-29 09:04:18 +0200

    ✨ Use navigation component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,28 @@
 import '@fontsource/roboto'
 
-import { CssBaseline, ThemeProvider } from '@material-ui/core'
+import {
+  Container,
+  CssBaseline,
+  ThemeProvider,
+  Toolbar,
+} from '@material-ui/core'
+import { observer } from 'mobx-react'
 import { FC } from 'react'
 
+import app from 'store/App'
+
+import Navigation from './components/navigation/Navigation'
 import theme from './theme'
 
 const App: FC = () => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
+      <Navigation {...app.navigation} />
+      <Toolbar />
+      <Container maxWidth="sm">coming soon...</Container>
     </ThemeProvider>
   )
 }
 
-export default App
+export default observer(App)
