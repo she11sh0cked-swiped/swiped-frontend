80872c15ad6ee2554ef9842d2ebeb6133075c64d
she11sh0cked <22623152+she11sh0cked@users.noreply.github.com>
2021-04-02 00:42:19 +0200

    🎨 Structural changes ✨ Add send vote handler

--- src/containers/swiping/components/deck/Deck.tsx
+++ src/containers/swiping/components/deck/Deck.tsx
@@ -32,32 +32,50 @@ interface IProps {
   registerControls(controls: IControls): void
 }
 
-const Deck: FC<IProps> = ({ registerControls }) => {
+interface ICard {
+  data: IMovie
+  i: number
+  visible: boolean
+  like?: boolean
+}
+
+const Deck: FC<IProps> = ({ groupId, registerControls }) => {
   const classes = useStyles()
 
-  const [cards, setCards] = useState(mock.movies())
-  const [currentIndex, setCurrentIndex] = useState(cards.length - 1)
+  const [movies, setMovies] = useState(mock.movies())
+  const [cards, setCards] = useState<ICard[]>([])
+
+  useEffect(() => {
+    setCards(movies.map((movie, i) => ({ data: movie, i, visible: true })))
+  }, [movies])
+
+  const currentCard = useMemo(
+    () =>
+      cards
+        .slice()
+        .reverse()
+        .find((card) => card.like == null),
+    [cards]
+  )
 
   // Create a bunch of springs using the helpers above
-  const [props, refs] = useSprings(cards.length, (i) => ({
-    ...to(i),
-    from: from(),
-  }))
-
-  const currentRef = useMemo(
-    () => refs.current[currentIndex],
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [refs.current[currentIndex]]
+  const [props, refs] = useSprings(
+    cards.length,
+    (i) => ({
+      ...to(i),
+      from: from(),
+    }),
+    [movies]
   )
+
+  const currentRef = currentCard != null ? refs.current[currentCard.i] : null
 
   const handleVote = useCallback(
     (like: boolean, animation: Partial<{ rot: number }> = {}) => {
-      if (currentIndex < 0) {
+      if (currentCard == null || currentRef == null) {
         refs.stop()
         return
       }
-
-      console.log(like ? '+' : '-', currentIndex)
 
       const dir = like ? 1 : -1
       animation.rot = animation.rot ?? 10 * dir
@@ -75,27 +93,38 @@ const Deck: FC<IProps> = ({ registerControls }) => {
         })
         .then(() => {
           setCards((prevCards) => {
-            console.log('x', prevCards.length - 1)
-            return prevCards.slice(0, -1)
+            prevCards[currentCard.i].visible = false
+            return prevCards.slice()
           })
         })
 
-      setCurrentIndex((prevCurrentIndex) => prevCurrentIndex - 1)
+      setCards((prevCards) => {
+        prevCards[currentCard.i].like = like
+        return prevCards.slice()
+      })
     },
-    [currentIndex, currentRef, refs]
+    [currentCard, currentRef, refs]
   )
+
+  const handleSendVote = useCallback(() => {
+    const vote = {
+      groupId,
+      votes: cards.map((card) => ({
+        like: card.like,
+        movieId: card.data.id,
+      })),
+    }
+
+    return new Promise<void>((resolve) => {
+      console.log(vote)
+      resolve()
+    })
+  }, [cards, groupId])
 
   // Create a gesture, we're interested in down-state, delta (current-pos - click-pos), direction and velocity
   const bind = useDrag(
-    ({
-      args: [index],
-      down,
-      movement: [xMove, yMove],
-      direction: [xDir],
-      velocity,
-    }) => {
-      // We're only interested in changing spring-data for the current spring
-      if (index !== currentIndex) return
+    ({ down, movement: [xMove, yMove], direction: [xDir], velocity }) => {
+      if (currentCard == null || currentRef == null) return
 
       const trigger = !down && velocity > 0.2 // If you flick hard enough and the finger is up it should trigger the card to fly out
       const dir = xDir < 0 ? -1 : 1 // Direction should either point left or right
@@ -113,9 +142,7 @@ const Deck: FC<IProps> = ({ registerControls }) => {
           },
           delay: undefined,
           rot: xMove / 100,
-
-          scale: down ? 1.1 : 1,
-          // Active cards lift up a bit
+          scale: down ? 1.1 : 1, // Active cards lift up a bit
           x: down ? xMove : 0,
           y: down ? yMove / 50 : 0, // Go back to zero if finger's up
         })
@@ -132,27 +159,34 @@ const Deck: FC<IProps> = ({ registerControls }) => {
   }, [handleVote, registerControls])
 
   useEffect(() => {
-    if (cards.length === 0) {
+    if (cards.length <= 0 || cards.some((card) => card.visible)) return
+
+    console.log('vote time')
+    void handleSendVote().then(() => {
       console.log('refill time')
-
-      const movies = mock.movies()
-
-      setCards(movies)
-      setCurrentIndex(movies.length - 1)
-    }
-  }, [cards.length])
+      refs.current.forEach((ref) => ref.set(from()))
+      setMovies(mock.movies())
+    })
+  }, [cards, handleSendVote, refs])
 
   // Now we're just mapping the animated values to our view, that's it. Btw, this component only renders once. :-)
   return (
     <div className={classes.root}>
       {props.map(({ rot, scale, x, y }, i) => {
+        const card = cards[i]
+
         return (
-          <animated.div key={i} style={{ x, y }}>
-            {/* This is the card itself, we're binding our gesture to it (and inject its index so we know which is which) */}
-            <animated.div {...bind(i)} style={{ rotateZ: rot, scale }}>
-              <Card key={cards[i].id} {...cards[i]} />
+          card.visible && (
+            <animated.div key={i} style={{ x, y }}>
+              {/* This is the card itself, we're binding our gesture to it (and inject its index so we know which is which) */}
+              <animated.div
+                {...(card.i === currentCard?.i ? bind() : {})} // We're only interested in changing spring-data for the current spring
+                style={{ rotateZ: rot, scale }}
+              >
+                <Card key={card.data.id} {...card.data} />
+              </animated.div>
             </animated.div>
-          </animated.div>
+          )
         )
       })}
     </div>
