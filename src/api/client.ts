2a04a8125f1d9355fc64ecb444e1719e45469ef3
she11sh0cked <22623152+she11sh0cked@users.noreply.github.com>
2021-04-14 14:39:52 +0200

    ✨ Add authLink to dynamically set the authorization header

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,4 +1,5 @@
 import { ApolloClient, from, HttpLink, InMemoryCache } from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
 import { onError } from '@apollo/client/link/error'
 
 const errorLink = onError(({ graphQLErrors, networkError }) => {
@@ -10,13 +11,25 @@ const errorLink = onError(({ graphQLErrors, networkError }) => {
   if (networkError) console.log(`[Network error]`, networkError)
 })
 
+const authLink = setContext(
+  (_, { headers }: { headers: Record<string, string> }) => {
+    const token = sessionStorage.getItem('token')
+    return {
+      headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : undefined,
+      },
+    }
+  }
+)
+
 const apolloClient = new ApolloClient({
   cache: new InMemoryCache(),
   defaultOptions: {
     mutate: { errorPolicy: 'all' },
     query: { errorPolicy: 'all' },
   },
-  link: from([errorLink, new HttpLink()]),
+  link: from([authLink, errorLink, new HttpLink()]),
   uri: '/graphql',
 })
 
