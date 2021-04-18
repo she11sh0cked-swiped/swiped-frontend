2c1a9ff360bea14855059e17f1fd55a97f179f42
she11sh0cked <22623152+she11sh0cked@users.noreply.github.com>
2021-04-18 17:10:57 +0200

    🎨 Move routes into a component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,78 +1,19 @@
 import '@fontsource/roboto'
 
 import { Container, CssBaseline, Toolbar } from '@material-ui/core'
-import { FC, lazy, Suspense, useEffect } from 'react'
-import { Redirect, Route, Switch, useHistory } from 'react-router-dom'
-
-import Loading from 'containers/loading/Loading'
+import { FC } from 'react'
 
 import Navigation from './components/navigation/Navigation'
-
-const noRedirect: Record<string, boolean> = {
-  '/login': true,
-  '/register': true,
-}
+import Routes from './components/routes/Routes'
 
 const App: FC = () => {
-  const history = useHistory()
-
-  useEffect(() => {
-    const isTokenEmpty = localStorage.getItem('token') == null
-    const isRedirect = !noRedirect[history.location.pathname]
-    if (isTokenEmpty && isRedirect) history.replace('/login')
-  }, [history])
-
   return (
     <>
       <CssBaseline />
       <Navigation />
       <Toolbar />
       <Container maxWidth="sm">
-        <Suspense fallback={<Loading />}>
-          <Switch>
-            <Route
-              component={lazy(() => import('containers/swiping/Swiping'))}
-              exact
-              path="/"
-            />
-            <Route
-              component={lazy(() => import('containers/error/Error'))}
-              exact
-              path="/404"
-            />
-            <Route
-              component={lazy(() => import('containers/login/Login'))}
-              exact
-              path="/login"
-            />
-            <Route
-              component={lazy(() => import('containers/register/Register'))}
-              exact
-              path="/register"
-            />
-            <Route
-              component={lazy(() => import('containers/groups/Groups'))}
-              exact
-              path="/groups"
-            />
-            <Route
-              component={lazy(() => import('containers/groupEdit/GroupEdit'))}
-              exact
-              path="/g/new"
-            />
-            <Route
-              component={lazy(() => import('containers/group/Group'))}
-              exact
-              path="/g/:groupId"
-            />
-            <Route
-              component={lazy(() => import('containers/groupEdit/GroupEdit'))}
-              exact
-              path="/g/:groupId/edit"
-            />
-            <Redirect to="/404" />
-          </Switch>
-        </Suspense>
+        <Routes />
       </Container>
     </>
   )
---
+++ src/app/components/routes/Routes.tsx
@@ -0,0 +1,71 @@
+import { FC, lazy, Suspense, useEffect } from 'react'
+import { Redirect, Route, Switch, useHistory } from 'react-router-dom'
+
+import Loading from 'containers/loading/Loading'
+
+const noRedirect: Record<string, boolean> = {
+  '/login': true,
+  '/register': true,
+}
+
+const Routes: FC = () => {
+  const history = useHistory()
+
+  useEffect(() => {
+    const isTokenEmpty = localStorage.getItem('token') == null
+    const isRedirect = !noRedirect[history.location.pathname]
+    const isSnap = navigator.userAgent === 'ReactSnap'
+
+    if (isTokenEmpty && isRedirect && !isSnap) history.replace('/login')
+  }, [history])
+
+  return (
+    <Suspense fallback={<Loading />}>
+      <Switch>
+        <Route
+          component={lazy(() => import('containers/swiping/Swiping'))}
+          exact
+          path="/"
+        />
+        <Route
+          component={lazy(() => import('containers/error/Error'))}
+          exact
+          path="/404"
+        />
+        <Route
+          component={lazy(() => import('containers/login/Login'))}
+          exact
+          path="/login"
+        />
+        <Route
+          component={lazy(() => import('containers/register/Register'))}
+          exact
+          path="/register"
+        />
+        <Route
+          component={lazy(() => import('containers/groups/Groups'))}
+          exact
+          path="/groups"
+        />
+        <Route
+          component={lazy(() => import('containers/groupEdit/GroupEdit'))}
+          exact
+          path="/g/new"
+        />
+        <Route
+          component={lazy(() => import('containers/group/Group'))}
+          exact
+          path="/g/:groupId"
+        />
+        <Route
+          component={lazy(() => import('containers/groupEdit/GroupEdit'))}
+          exact
+          path="/g/:groupId/edit"
+        />
+        <Redirect to="/404" />
+      </Switch>
+    </Suspense>
+  )
+}
+
+export default Routes
