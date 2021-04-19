4bbf65ccc3bea7297a56907a63030536a8db987e
she11sh0cked <22623152+she11sh0cked@users.noreply.github.com>
2021-04-19 12:37:00 +0200

    ✨ Only show the edit icon if we are the owner

--- src/containers/group/Group.generated.ts
+++ src/containers/group/Group.generated.ts
@@ -12,7 +12,18 @@ export type GroupQuery = (
   { __typename?: 'Query' }
   & { group_findById?: Types.Maybe<(
     { __typename?: 'group' }
-    & Pick<Types.Group, '_id'>
+    & Pick<Types.Group, '_id' | 'ownerId'>
+  )> }
+);
+
+export type UserQueryVariables = Types.Exact<{ [key: string]: never; }>;
+
+
+export type UserQuery = (
+  { __typename?: 'Query' }
+  & { user_findMe?: Types.Maybe<(
+    { __typename?: 'user' }
+    & Pick<Types.User, '_id'>
   )> }
 );
 
@@ -21,6 +32,7 @@ export const GroupDocument = gql`
     query group($id: MongoID!) {
   group_findById(_id: $id) {
     _id
+    ownerId
   }
 }
     `;
@@ -52,3 +64,37 @@ export function useGroupLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<Grou
 export type GroupQueryHookResult = ReturnType<typeof useGroupQuery>;
 export type GroupLazyQueryHookResult = ReturnType<typeof useGroupLazyQuery>;
 export type GroupQueryResult = Apollo.QueryResult<GroupQuery, GroupQueryVariables>;
+export const UserDocument = gql`
+    query user {
+  user_findMe {
+    _id
+  }
+}
+    `;
+
+/**
+ * __useUserQuery__
+ *
+ * To run a query within a React component, call `useUserQuery` and pass it any options that fit your needs.
+ * When your component renders, `useUserQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useUserQuery({
+ *   variables: {
+ *   },
+ * });
+ */
+export function useUserQuery(baseOptions?: Apollo.QueryHookOptions<UserQuery, UserQueryVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<UserQuery, UserQueryVariables>(UserDocument, options);
+      }
+export function useUserLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<UserQuery, UserQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<UserQuery, UserQueryVariables>(UserDocument, options);
+        }
+export type UserQueryHookResult = ReturnType<typeof useUserQuery>;
+export type UserLazyQueryHookResult = ReturnType<typeof useUserLazyQuery>;
+export type UserQueryResult = Apollo.QueryResult<UserQuery, UserQueryVariables>;
--- src/containers/group/Group.tsx
+++ src/containers/group/Group.tsx
@@ -1,12 +1,12 @@
 import { ArrowBack, Edit } from '@material-ui/icons'
-import { FC, useEffect } from 'react'
+import { FC, useEffect, useMemo } from 'react'
 import { RouteComponentProps } from 'react-router-dom'
 
 import Loading from 'containers/loading/Loading'
 import app from 'store/App'
 
 import List from './components/list/List'
-import { useGroupQuery } from './Group.generated'
+import { useGroupQuery, useUserQuery } from './Group.generated'
 
 type IProps = RouteComponentProps<{ groupId: string }>
 
@@ -23,20 +23,37 @@ const Group: FC<IProps> = ({
     variables: { id: groupId },
   })
 
+  const userResult = useUserQuery()
+
+  const group = groupResult.data?.group_findById
+  const user = userResult.data?.user_findMe
+
+  const isLoading = useMemo(() => groupResult.loading || userResult.loading, [
+    groupResult.loading,
+    userResult.loading,
+  ])
+
+  const isOwner = useMemo(
+    () => (isLoading ? false : group?.ownerId === user?._id),
+    [group?.ownerId, isLoading, user?._id]
+  )
+
   useEffect(() => {
     app.navigation = {
       left: {
         icon: ArrowBack,
         to: '/groups',
       },
-      right: {
-        icon: Edit,
-        to: `/g/${groupId}/edit`,
-      },
+      right: isOwner
+        ? {
+            icon: Edit,
+            to: `/g/${groupId}/edit`,
+          }
+        : undefined,
     }
-  }, [groupId])
+  }, [groupId, isOwner])
 
-  if (groupResult.loading) return <Loading />
+  if (isLoading) return <Loading />
 
   return (
     <div>
